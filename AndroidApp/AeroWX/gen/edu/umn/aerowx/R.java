8208bc6e52e1a9dc5bf0fdee375b81f10451a287
Wayne Johnson <wdtj@yahoo.com>
2013-03-09 17:31:41 -0600

    Added method to GET to http://aerowx.dccmn.com/get_weather

--- AndroidApp/AeroWX/gen/edu/umn/aerowx/R.java
+++ AndroidApp/AeroWX/gen/edu/umn/aerowx/R.java
@@ -33,9 +33,9 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int AeroWX=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
--- AndroidApp/AeroWX/src/edu/umn/aerowx/MainActivity.java
+++ AndroidApp/AeroWX/src/edu/umn/aerowx/MainActivity.java
@@ -1,15 +1,41 @@
 package edu.umn.aerowx;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+
+/**
+ * Main activity for AeroWX App
+ * 
+ * @author waynej
+ *
+ */
 
 public class MainActivity extends Activity {
 
+	/**
+	 * 
+	 */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+		String readHTTPServer = readHTTPServer();
     }
 
 
@@ -20,4 +46,32 @@ public class MainActivity extends Activity {
         return true;
     }
     
+	public String readHTTPServer() {
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(
+				"http://aerowx.dccmn.com/get_weather");
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
+				Log.e(MainActivity.class.toString(), "Failed to download file");
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
 }
