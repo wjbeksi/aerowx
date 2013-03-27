89b5f326aa966efab1b0741747fe94e066ac7210
Wayne Johnson <wdtj@yahoo.com>
2013-03-27 16:30:47 -0500

    Remove generated file

--- AndroidApp/AeroWX/gen/edu/umn/aerowx/R.java
+++ AndroidApp/AeroWX/gen/edu/umn/aerowx/R.java
@@ -24,47 +24,74 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080014;
-        public static final int dewLabel=0x7f080006;
-        public static final int dewpoint=0x7f080008;
-        public static final int forecastButton=0x7f080013;
-        public static final int pressLabel=0x7f080009;
-        public static final int pressure=0x7f08000a;
-        public static final int sky=0x7f080012;
-        public static final int skyLabel=0x7f080011;
+        public static final int action_settings=0x7f080020;
+        public static final int ceil=0x7f080012;
+        public static final int dewLabel=0x7f080016;
+        public static final int dewpoint=0x7f080017;
+        public static final int dewptRow=0x7f08000c;
+        public static final int forecastButton=0x7f08001f;
+        public static final int gfsTitle=0x7f080000;
+        public static final int high=0x7f080006;
+        public static final int highLabel=0x7f080005;
+        public static final int low=0x7f080008;
+        public static final int lowLabel=0x7f080007;
+        public static final int metarTitle=0x7f080013;
+        public static final int periodDate=0x7f080009;
+        public static final int periodTime=0x7f08000a;
+        public static final int precip=0x7f08000f;
+        public static final int pressLabel=0x7f080018;
+        public static final int pressure=0x7f080019;
+        public static final int sky=0x7f08000d;
+        public static final int skyLabel=0x7f08001e;
         public static final int station=0x7f080002;
         public static final int stationLabel=0x7f080001;
-        public static final int temp=0x7f080007;
-        public static final int tempLabel=0x7f080005;
-        public static final int textView1=0x7f080000;
+        public static final int temp=0x7f080015;
+        public static final int tempLabel=0x7f080014;
+        public static final int tempRow=0x7f08000b;
+        public static final int thund=0x7f080010;
         public static final int time=0x7f080004;
         public static final int timeLabel=0x7f080003;
-        public static final int vis=0x7f08000e;
-        public static final int visLabel=0x7f08000d;
-        public static final int wind=0x7f08000c;
-        public static final int windLabel=0x7f08000b;
-        public static final int wx=0x7f080010;
-        public static final int wxLabel=0x7f08000f;
+        public static final int vis=0x7f080011;
+        public static final int visLabel=0x7f08001b;
+        public static final int wind=0x7f08000e;
+        public static final int windLabel=0x7f08001a;
+        public static final int wx=0x7f08001d;
+        public static final int wxLabel=0x7f08001c;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_gfs=0x7f030000;
+        public static final int activity_metar=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int gf=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int AeroWX=0x7f050002;
+        public static final int Ceil=0x7f050014;
         public static final int Dewpoint=0x7f050006;
+        public static final int Dewpt=0x7f050010;
+        public static final int Forecast=0x7f05000c;
+        public static final int High=0x7f05000e;
+        public static final int Low=0x7f05000f;
+        public static final int Precip=0x7f050011;
         public static final int Pressure=0x7f050007;
         public static final int Sky=0x7f05000b;
+        /**   METAR field labels 
+         */
         public static final int Station=0x7f050003;
         public static final int Temp=0x7f050005;
+        public static final int Thund=0x7f050013;
         public static final int Time=0x7f050004;
+        public static final int Visi=0x7f050012;
         public static final int Visibility=0x7f050009;
         public static final int Weather=0x7f05000a;
         public static final int Wind=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        /**   GFS/MOS/MAV field labels 
+         */
+        public static final int title_activity_gfs=0x7f05000d;
     }
     public static final class style {
         /** 
