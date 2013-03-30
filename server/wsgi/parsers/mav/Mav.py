d2f845108a6e4ec446afc4931b7b7ffd69d9fa4b
William J Beksi <beksi@cs.umn.edu>
2013-03-30 00:39:53 -0500

    Add more fields and return a readable version of the decoded message.

--- server/wsgi/parsers/mav/Mav.py
+++ server/wsgi/parsers/mav/Mav.py
@@ -8,6 +8,7 @@ encoded by a single GFS MOS MAV message.
 """
 import re
 import json
+import string
 import pprint
 
 MAX_COLS = 22  # Number of columns in a MAV message
@@ -21,7 +22,9 @@ class ParserError(Exception):
 
 # Regular expressions to decode various groups of the MAV message 
 
-DT_RE = re.compile(r'DT\s+(.+)')
+STATION_RE = re.compile(r"([A-Z][A-Z0-9]{3})\s+")
+
+DT_RE = re.compile(r"DT\s+(.+)")
 
 HR_RE = re.compile(r"""HR\s+(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s
                             (\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s
@@ -71,17 +74,85 @@ WSP_RE = re.compile(r"""WSP\s+(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s
                               (\d+)\s(\d+)\s(\d+)""",
                               re.VERBOSE)
 
+TYP_RE = re.compile(r"""TYP\s+(\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s
+                              (\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s
+                              (\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s
+                              (\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s(\s*\w)\s
+                              (\s*\w)""",
+                              re.VERBOSE)
 
+SNW_RE = re.compile(r"""SNW\s\s(\s\s|\d*)\s(\s\s|\d*)\s(\s\s|\d*)\s
+                               (\s\s|\d*)\s(\s\s|\d*)\s(\s\s|\d*)\s
+                               (\s\s|\d*)\s(\s\s|\d*)\s(\s\s|\d*)\s
+                               (\s\s|\d*)\s(\s\s|\d*)\s(\s\s|\d*)\s
+                               (\s\s|\d*)\s(\s\s|\d*)\s(\s\s|\d*)\s
+                               (\s\s|\d*)\s(\s\s|\d*)\s(\s\s|\d*)\s
+                               (\s\s|\d*)\s(\s\s|\d*)\s(\s\s|\d*)""",
+                               re.VERBOSE)
 
+CIG_RE = re.compile(r"""CIG\s+(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)""",
+                              re.VERBOSE)
 
-# Translation of the cloud category codes
+VIS_RE = re.compile(r"""VIS\s+(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s(\s*\d+)\s
+                              (\s*\d+)""",
+                              re.VERBOSE)
 
-CLOUD = {"CL" : "clear", 
-         "FW" : "few > 0 to 2 octas",
-         "SC" : "scattered > 2 to 4 octas",
-         "BK" : "broken > 4 to < 8 octas",
-         "OV" : "overcast"}
-                          
+OBV_RE = re.compile(r"""OBV\s+(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s
+                              (\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s
+                              (\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s
+                              (\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s(\s*\w+)\s
+                              (\s*\w+)""",
+                              re.VERBOSE)
+
+# Translation of the category codes
+
+CEILING_HEIGHT = {"1" : "< 200 feet", 
+                  "2" : "200 - 400 feet",
+                  "3" : "500 - 900 feet",
+                  "4" : "1,000 - 1,900 feet",
+                  "5" : "2,000 - 3,000 feet",
+                  "6" : "3,100 - 6,500 feet",
+                  "7" : "6,600 - 12,000 feet",
+                  "8" : "> 12,000 feet or unlimited ceiling"}
+
+VISIBILITY = {"1" : "< 1/2 miles", 
+              "2" : "1/2 - < 1 miles",
+              "3" : "1 - < 2 miles",
+              "4" : "2 - < 3 miles",
+              "5" : "3 - 5 miles",
+              "6" : "6 miles",
+              "7" : "> 6 miles"}
+ 
+SKY_COVER = {"CL" : "clear", 
+             "FW" : "few > 0 to 2 octas",
+             "SC" : "scattered > 2 to 4 octas",
+             "BK" : "broken > 4 to < 8 octas",
+             "OV" : "overcast"}
+
+OBSTRUCTION_TO_VISION = {"N"  : "none", 
+                         "HZ" : "haze, smoke, dust",
+                         "BR" : "mist (fog with visibility >= 5/8 mile)",
+                         "FG" : "fog or ground fog (visibility < 5/8 mile)",
+                         "BL" : "blowing dust, sand, snow"}
+
+SNOWFALL_AMOUNT = {"0" : "no snow or a trace expected", 
+                   "1" : "> a trace to < 2 inches",
+                   "2" : "2 to < 4 inches",
+                   "4" : "4 to 6 inches",
+                   "6" : "6 to < 8 inches",
+                   "8" : ">= 8 inches"}
+
+PRECIPITATION_TYPE = {"S" : "pure snow or snow grains", 
+                      "Z" : "freezing rain/drizzle, ice pellets, or anything mixed with freezing precip",
+                      "R" : "pure rain/drizzle or rain mixed with snow"}
+
 # MAV report objects
 
 debug = False
@@ -92,16 +163,24 @@ class Mav(object):
     def __init__(self, message):
         """Parse the raw MAV message"""
         self.station_id = None  # 4-character ICAO station code
-        self.dt = []            # Day of the month, denoted by the standard three or four letter abreviation
+        self.dt = []            # Day of the month, denoted by the standard three or four letter abbreviation
         self.hr = []            # Hour of the day in UTC 
-        self.nx = []            # Nighttime minimum/daytime maxium surface temperatures 
+        self.nx = []            # Nighttime minimum/daytime maximum surface temperatures 
         self.tmp = []           # Surface temperature valid at that hour 
-        self.dpt = []           # Surface dewpoint valid at that hour 
-        self.cld = []           # Forecast cataegories of total sky cover valid at that hour 
+        self.dpt = []           # Surface dew point valid at that hour 
+        self.cld = []           # Forecast categories of total sky cover valid at that hour 
         self.wdr = []           # Forecasts of the 10-meter wind direction at the hour, given in tens of degrees 
         self.wsp = []           # Forecasts of the 10-meter wind speed at the hour, given in knots 
+        self.typ = []           # Conditional precipitation type at the hour 
+        self.snw = []           # Snowfall categorical forecasts during a 24-h period ending at the indicated time 
+        self.cig = []           # Ceiling height categorical forecasts at the hour 
+        self.vis = []           # Visibility categorical forecasts at the hour 
+        self.obv = []           # Obstruction to vision categorical forecasts at the hour 
 
         try:
+            m = STATION_RE.search(message)
+            if m:
+                self._handle_station(m)
             m = DT_RE.search(message)
             if m:
                 self._handle_dt(m)
@@ -126,11 +205,203 @@ class Mav(object):
             m = WSP_RE.search(message)
             if m:
                 self._handle_wsp(m)
+            m = TYP_RE.search(message)
+            if m:
+                self._handle_typ(m)
+                print 'type: ', self.typ
+            m = SNW_RE.search(message)
+            if m:
+                self._handle_snw(m)
+                print 'snw: ', self.snw
+            else:
+                print 'NO SNW MATCH'
+            m = CIG_RE.search(message)
+            if m:
+                self._handle_cig(m)
+            m = VIS_RE.search(message)
+            if m:
+                self._handle_vis(m)
+            m = OBV_RE.search(message)
+            if m:
+                self._handle_obv(m)
 
            
         except Exception, err:
             #raise ParserError(handler.__name__+" failed while processing '"+code+"'\n"+string.join(err.args))
             raise err
+
+    def __str__(self):
+        return self.string()
+
+    def _handle_station(self, m):
+        """
+        Extract the station ID.
+        """
+        self.station_id = m.group(1)
+
+    
+    def _handle_dt(self, m):
+        """
+        Extract the date fields.
+        """
+        self.dt = re.findall('\w+\s+\d+', m.group(1))
+
+    def _handle_hr(self, m):
+        """
+        Extract the hour of the day fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.hr.append(m.group(i))
+
+    def _handle_nx(self, m):
+        """
+        Extract the minimum/maximum surface temperature fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.nx.append(m.group(i))
+
+    def _handle_tmp(self, m):
+        """
+        Extract the surface temperature fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.tmp.append(m.group(i))
+
+    def _handle_dpt(self, m):
+        """
+        Extract the surface dew point fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.dpt.append(m.group(i))
+
+    def _handle_cld(self, m):
+        """
+        Extract the sky cover fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.cld.append(m.group(i))
+
+    def _handle_wdr(self, m):
+        """
+        Extract the wind direction fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.wdr.append(m.group(i))
+
+    def _handle_wsp(self, m):
+        """
+        Extract the wind speed fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.wsp.append(m.group(i))
+
+    def _handle_typ(self, m):
+        """
+        Extract the precipitation type fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.typ.append(m.group(i).strip())
+
+    def _handle_snw(self, m):
+        """
+        Extract the snowfall fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.snw.append(m.group(i))
+
+    def _handle_cig(self, m):
+        """
+        Extract the ceiling height fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.cig.append(m.group(i).strip())
+
+    def _handle_vis(self, m):
+        """
+        Extract the visibility fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.vis.append(m.group(i).strip())
+
+    def _handle_obv(self, m):
+        """
+        Extract the obstruction to view fields.
+        """
+        for i in range(1,MAX_COLS):
+            self.obv.append(m.group(i).strip())
+
+    def string(self):
+        """
+        Return a human readable version of the decoded message.
+        """
+        lines = []
+        if self.station_id:
+            lines.append("station: %s" % self.station_id)
+
+        ndays = len(self.dt)
+        nhours = len(self.hr)
+        h = 0
+        for d in range(ndays):
+            lines.append("date: %s" % self.dt[d])
+            while 1:
+                lines.append("\thour: %s" % self.hr[h])
+                self.attach_string_fields(lines, h)
+                h += 1
+                if h == nhours or self.hr[h] == '00':
+                    break
+
+        return string.join(lines,"\n")
+    
+    def attach_string_fields(self, lines, h):
+        if self.nx[h] != '  ':
+            lines.append("\t\tnighttime minimum/daytime maximum surface temperature: %s" % self.nx[h])
+        lines.append("\t\tsurface temperature: %s" % self.tmp[h])
+        lines.append("\t\tsurface dew point: %s" % self.dpt[h])
+        lines.append("\t\tsky cover: %s" % SKY_COVER[self.cld[h]])               
+        lines.append("\t\twind direction: %s" % self.wdr[h])
+        lines.append("\t\twind speed: %s" % self.wsp[h])
+        lines.append("\t\tprecipitation type: %s" % PRECIPITATION_TYPE[self.typ[h]])
+        #if self.snw[h] != '  ':
+        #    lines.append("\t\tsnowfall: %s" % SNOWFALL_AMOUNT[self.snw[h]])
+        lines.append("\t\tceiling height: %s" % CEILING_HEIGHT[self.cig[h]])
+        lines.append("\t\tvisibility: %s" % VISIBILITY[self.vis[h]])
+        lines.append("\t\tobstruction to vision: %s" % OBSTRUCTION_TO_VISION[self.obv[h]])
+
+    #def json(self):
+    #    """
+    #    Return a JSON version of the decoded message.
+    #    """
+    #    lines = []
+    #    if self.station_id:
+    #        lines.append("station: %s" % self.station_id)
+
+    #    ndays = len(self.dt)
+    #    nhours = len(self.hr)
+    #    h = 0
+    #    for d in range(ndays):
+    #        lines.append("date: %s" % self.dt[d])
+    #        while 1:
+    #            lines.append("hour: %s" % self.hr[h])
+    #            self.attach_json_fields(lines, h)
+    #            h += 1
+    #            if h == nhours or self.hr[h] == '00':
+    #                break
+
+    #    return json.dumps({'mav' : lines}, sort_keys=True, indent=4, separators=(',', ': '))
+        #return json.dumps({'mav': lines})
+          
+    #def attach_json_fields(self, lines, h):
+    #    if self.nx[h] != '  ':
+    #        lines.append("nighttime minimum/daytime maximum surface temperature: %s" % self.nx[h])
+    #    lines.append("surface temperature: %s" % self.tmp[h])
+    #    lines.append("surface dew point: %s" % self.dpt[h])
+    #    lines.append("sky cover: %s" % SKY_COVER[self.cld[h]])               
+    #    lines.append("wind direction: %s" % self.wdr[h])
+    #    lines.append("wind speed: %s" % self.wsp[h])
+    #    lines.append("precipitation type: %s" % PRECIPITATION_TYPE[self.typ[h]])
+    #    lines.append("ceiling height: %s" % CEILING_HEIGHT[self.cig[h]])
+    #    lines.append("visibility: %s" % VISIBILITY[self.vis[h]])
+    #    lines.append("obstruction to vision: %s" % OBSTRUCTION_TO_VISION[self.obv[h]])
 
     @staticmethod
     def createEmptyPeriodArray():
@@ -142,9 +413,8 @@ class Mav(object):
                           'dewpoint' : 'None',
                           'cover' : 'None',
                           'wind' : {'direction' : 'None',
-                                    'speed' : 'None',
-                                    'gust' : 'None',
-                                    },
+                                     'speed' : 'None',
+                                     'gust' : 'None'},
                           'pop6' : 'None',
                           'pop12' : 'None',
                           'qpf12' : 'None',
@@ -156,84 +426,18 @@ class Mav(object):
                           'snow' : 'None',
                           'visibility' : 'None',
                           'obscurity' : 'None',
-                          'ceiling' : 'None'
-                          }
+                          'ceiling' : 'None'}
         return periods
 
     def json(self):
-
+        """
+        Return a JSON version of the decoded message.
+        """
         periodArray = Mav.createEmptyPeriodArray()
         report = {'wxid' : self.station_id,
                   'time' : 'None',
                   'high' : 'None',
                   'low' : 'None',
-                  'periods' : periodArray
-                  }
-
+                  'periods' : periodArray}
         #pprint.pprint(report)
         return json.dumps({'mav': report}) 
-
-
-
-    def _handle_dt(self, m):
-        """
-        Extract the date fields.
-        """
-        self.dt = re.findall('\w+\s+\d+', m.group(1))
-        print self.dt
-
-    def _handle_hr(self, m):
-        """
-        Extract the hour of the day fields.
-        """
-        for i in range(1,MAX_COLS):
-            self.hr.append(m.group(i))
-        print self.hr
-
-    def _handle_nx(self, m):
-        """
-        Extract the minimum/maxium surface temperature fields.
-        """
-        for i in range(1,MAX_COLS):
-            self.nx.append(m.group(i))
-        print self.nx
-
-    def _handle_tmp(self, m):
-        """
-        Extract the surface temperature fields.
-        """
-        for i in range(1,MAX_COLS):
-            self.tmp.append(m.group(i))
-        print self.tmp
-
-    def _handle_dpt(self, m):
-        """
-        Extract the surface dewpoint fields.
-        """
-        for i in range(1,MAX_COLS):
-            self.dpt.append(m.group(i))
-        print self.dpt
-
-    def _handle_cld(self, m):
-        """
-        Extract the sky cover fields.
-        """
-        for i in range(1,MAX_COLS):
-            self.cld.append(m.group(i))
-        print self.cld
-
-    def _handle_wdr(self, m):
-        """
-        Extract the wind direction fields.
-        """
-        for i in range(1,MAX_COLS):
-            self.wdr.append(m.group(i))
-        print self.wdr
-
-    def _handle_wsp(self, m):
-        """
-        Extract the wind speed fields.
-        """
-        for i in range(1,MAX_COLS):
-            self.wsp.append(m.group(i))
-        print self.wsp
