be7ceee50b7682c640326f4e03d5059ecfec3ee0
Wayne Johnson <wdtj@yahoo.com>
2013-04-01 21:33:57 -0500

    Why can't I remove this!!!!

--- AndroidApp/AeroWX/gen/edu/umn/aerowx/R.java
+++ AndroidApp/AeroWX/gen/edu/umn/aerowx/R.java
@@ -8,12 +8,6 @@
 package edu.umn.aerowx;
 
 public final class R {
-    public static final class array {
-        public static final int pref_example_list_titles=0x7f060000;
-        public static final int pref_example_list_values=0x7f060001;
-        public static final int pref_sync_frequency_titles=0x7f060002;
-        public static final int pref_sync_frequency_values=0x7f060003;
-    }
     public static final class attr {
     }
     public static final class dimen {
@@ -30,49 +24,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
-        public static final int station=0x7f090002;
-        public static final int stationLabel=0x7f090001;
-        public static final int temp=0x7f090006;
-        public static final int tempLabel=0x7f090005;
-        public static final int textView1=0x7f090000;
-        public static final int time=0x7f090004;
-        public static final int timeLabel=0x7f090003;
+        public static final int action_settings=0x7f08000c;
+        public static final int baseURLLabel=0x7f080009;
+        public static final int baseURLSetting=0x7f08000a;
+        public static final int save=0x7f08000b;
+        public static final int settingsTitle=0x7f080007;
+        public static final int station=0x7f080002;
+        public static final int stationLabel=0x7f080001;
+        public static final int stationSetting=0x7f080008;
+        public static final int temp=0x7f080006;
+        public static final int tempLabel=0x7f080005;
+        public static final int textView1=0x7f080000;
+        public static final int time=0x7f080004;
+        public static final int timeLabel=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_settings=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int AeroWX=0x7f050002;
+        public static final int Save=0x7f050008;
         public static final int Station=0x7f050003;
         public static final int Temp=0x7f050005;
         public static final int Time=0x7f050004;
+        public static final int URL=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int pref_default_display_name=0x7f05000b;
-        public static final int pref_description_social_recommendations=0x7f050009;
-        /**  Example settings for Data & Sync 
-         */
-        public static final int pref_header_data_sync=0x7f05000d;
-        /**  Strings related to Settings 
- Example General settings 
-         */
-        public static final int pref_header_general=0x7f050007;
-        /**  Example settings for Notifications 
-         */
-        public static final int pref_header_notifications=0x7f050010;
-        public static final int pref_ringtone_silent=0x7f050013;
-        public static final int pref_title_add_friends_to_messages=0x7f05000c;
-        public static final int pref_title_display_name=0x7f05000a;
-        public static final int pref_title_new_message_notifications=0x7f050011;
-        public static final int pref_title_ringtone=0x7f050012;
-        public static final int pref_title_social_recommendations=0x7f050008;
-        public static final int pref_title_sync_frequency=0x7f05000e;
-        public static final int pref_title_system_sync_settings=0x7f05000f;
-        public static final int pref_title_vibrate=0x7f050014;
         public static final int title_activity_settings=0x7f050006;
     }
     public static final class style {
@@ -97,10 +78,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
